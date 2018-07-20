4d490d898481bbb5d297501715202fdcca8d7e2d
U-BRANDCONSTANTIN\brand <brand_constantin@hotmail.com>
2018-07-20 19:04:57 +0200

    60 herramientas de desarrollo

--- 60_developer_tools/public_html/lib/curso5.js
+++ 60_developer_tools/public_html/lib/curso5.js
@@ -23,3 +23,55 @@ var swiper = new Swiper('.slideshow2', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+//sweetalert ////////////////////
+swal({
+  title: "Good job!",
+  text: "You clicked the button!",
+  icon: "success",
+//  timer: 2000
+});
+
+
+//modal de bootstrap /////////////////////
+$("#myModal").modal();
+setTimeout(function(){
+    $("#myModal").modal('hide');
+}, 3000);
+
+// the date picker //////////////////
+$('.fecha').pickadate({
+    format: 'yyyy/mm/dd'
+});
+
+$("#botObtener").on("click", function(){
+   var valor = $("#txtFecha").val();
+   console.log(valor);
+});
+
+
+//chosen ///////////////
+$(".chosen-select").chosen({
+    no_results_text: "Oops, no he encontrado nada"
+});
+
+
+//complexify ///////////////
+$("#pass").complexify({}, function(valid, complexity){
+    console.log(valid, complexity);
+//    alert("Password complexity: " + complexity);
+
+    var $progressBar = $("#progressBar");
+    
+    $progressBar.css('width', complexity + '%');
+    
+    if(complexity >= 40){
+        $progressBar
+                .addClass('progress-bar-success')
+                .removeClass('progress-bar-danger');
+    }else{
+        $progressBar
+                .addClass('progress-bar-danger')
+                .removeClass('progress-bar-success');
+    }
+});
