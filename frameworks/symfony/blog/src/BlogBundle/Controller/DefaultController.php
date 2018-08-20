31aea16cac0fe2ba48f025f7ffe5e92ca0df58ce
U-BRANDCONSTANTIN\brand <brand_constantin@hotmail.com>
2018-08-20 20:00:52 +0200

    blog en symfony

---
+++ frameworks/symfony/blog/src/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller {
+
+    public function indexAction() {
+        //listar todas las entradas
+        $em = $this->getDoctrine()->getManager();
+        $entry_repo = $em->getRepository("BlogBundle:Entry");
+        $entries = $entry_repo->findAll();
+
+        foreach ($entries as $entry) {
+            echo $entry->getTitle() . "<br/>";
+            echo $entry->getCategory()->getName() . "<br/>";
+            echo $entry->getUser()->getName() . "<br/>";
+
+            $tags = $entry->getEntryTag();
+            foreach ($tags as $tag) {
+                echo $tag->getTag()->getName() . " - ";
+            }
+            echo "<hr/>";
+        }
+
+        die();
+        return $this->render('BlogBundle:Default:index.html.twig');
+    }
+
+}
